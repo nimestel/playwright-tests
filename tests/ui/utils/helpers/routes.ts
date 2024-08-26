145dda428211a4e756991ee558326c3f8df578fb
Tatiana <cherv.tatiana@gmail.com>
2024-08-26 11:37:52 +0400

    fix: change default activity group slug

--- tests/ui/utils/helpers/routes.ts
+++ tests/ui/utils/helpers/routes.ts
@@ -2,6 +2,6 @@ import { Locale } from './locales';
 
 export default {
     getLocale: (locale: Locale) => `https://**/_next/data/**/${locale.url}.json`,
-    getActivityGroup: (activityGroupSlug: string = 'events') =>
+    getActivityGroup: (activityGroupSlug: string = 'active') =>
         `https://**/activities/${activityGroupSlug}.json?slug=${activityGroupSlug}`
 };
